3c58378371ebd029db740a4c8355878943cd9408
majunchao <354488954@qq.com>
2018-06-19 22:58:07 +0800

    mockjs练习
    mockjs练习

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as mock from './mock.js'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+Vue.filter('getYMD',function(input) {
+	return input.split(' ')[0];
+});
